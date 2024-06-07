f926298103e52114300380a58135d5135a484ec5
tr7zw <tr7zw@live.de>
2024-06-07 23:43:50 +0200

    Improve Firstperson support, update Loom, update Gradle

--- common/src/main/java/net/bettercombat/compatibility/FirstPersonModelHelper.java
+++ common/src/main/java/net/bettercombat/compatibility/FirstPersonModelHelper.java
@@ -1,13 +1,14 @@
 package net.bettercombat.compatibility;
 
 import dev.tr7zw.firstperson.api.FirstPersonAPI;
+import dev.tr7zw.firstperson.FirstPersonModelCore;
 
 import java.util.function.Supplier;
 
 public class FirstPersonModelHelper {
     public static Supplier<Boolean> isDisabled() {
         return (() -> {
-            return !FirstPersonAPI.isEnabled();
+            return !(FirstPersonAPI.isEnabled() && !FirstPersonModelCore.instance.getLogicHandler().hideArmsAndItems());
         });
     }
 }
